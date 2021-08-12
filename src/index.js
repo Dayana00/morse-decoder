a00f18e49e63ee80ee4e990dac29812aa06ef4ff
Dayana00 <70838027+Dayana00@users.noreply.github.com>
2021-08-12 18:49:54 +0300

    feat: add answer in index.js

--- src/index.js
+++ src/index.js
@@ -35,10 +35,48 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*****':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+  let exprChange = [];
+  let exprChangeBreak = []
+  let code = '';
+  let size = 5; 
+    
+
+  for ( let i = 0; i < expr.length; i= i + 2) {
+    exprChange.push(expr[i] + expr[i+1])
+  }
+
+  for (let i = 0; i < exprChange.length; i++) {
+
+    if ( exprChange[i]=== '10') {
+      exprChange.splice(i, 1, '.')
+    } else if (exprChange[i] === '11') {
+      exprChange.splice(i, 1, '-')
+    } else if (exprChange[i]=== '00') {
+      exprChange.splice(i, 1, '')
+    } else if (exprChange[i]=== '**') {
+      exprChange.splice(i, 1, '*')
+    }
+    
+  }
+
+  for (let i = 0; i < Math.ceil(exprChange.length/size); i++){
+
+    exprChangeBreak[i] = exprChange.slice((i*size), (i*size) + size).join("")
+
+    for ( let key in MORSE_TABLE) {
+      if(key == exprChangeBreak[i]) {
+        code += MORSE_TABLE[key]
+      }
+    }
+
+  }
+
+
+  return code
 }
 
 module.exports = {
